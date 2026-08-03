d03b98f96c05101b819f499a2bbfff8018cee9f0
Lighton <laitonkamanga@gmail.com>
2026-08-03 14:20:14 +0200

    Testing variables

--- FirstReactApp/src/App.jsx
+++ FirstReactApp/src/App.jsx
@@ -6,9 +6,28 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+   const name = "Lighton";
+   const x = 15;
+   const y = 24;
+   const names = ['Peter', 'Mike', 'John', 'Maria'];
+   const loggedIn = true;
 
-  return 1
-   
+   if(loggedIn){
+    return <h1 className=''>Hello {name}</h1>;
+   }
+
+  return (
+    <>
+      <div className='text-5xl'>All is well</div>
+      <p>Hello {}</p>
+      <p>
+        The sum of {x} and {y} is {x+y}
+      </p>
+      <ul>
+        
+      </ul>
+    </>
+  )
 }
 
 export default App
